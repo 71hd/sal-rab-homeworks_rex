a2f91ac43a6ed6e8b316216666d7fbed2f334437
Вадим Смирнов <Rex220976@gmail.com>
2022-05-07 11:26:03 +0300

    task2

--- task_2/solution.js
+++ task_2/solution.js
@@ -8,14 +8,14 @@ function calcShipping(sum, min, shipping) {
     let shippingSum; // расчет стоимости доставки
     if (productsSum == 0) {
         shippingSum == 0;
-        console.log ('Товар в корзине отсутствует - стоимость доставки 0 руб.');
+        //console.log ('Товар в корзине отсутствует - стоимость доставки 0 руб.');
     } else if (productsSum >= freeShippingMinSum) {
         shippingSum == 0;
-        console.log ('Доставка бесплатная');
+        //console.log ('Доставка бесплатная');
     } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
         shippingSum = shippingPrice;
-        console.log ('Товара в корзине не достаточно для бесплатной доставки. Стоимость доставки: '
-         + shippingPrice + ' руб.');
+        //console.log ('Товара в корзине не достаточно для бесплатной доставки. Стоимость доставки: '
+        // + shippingPrice + ' руб.');
     }
 
     // создайте переменную shippingSum
