7dcc50090fe9ad45c6098c36da03fc56efe0c35a
Вадим Смирнов <Rex220976@gmail.com>
2022-05-22 21:36:25 +0300

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,8 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+
+    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
 
@@ -47,7 +48,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
