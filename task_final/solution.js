4d639f64e126310658e838e93259f0f23c70744e
Вадим Смирнов <Rex220976@gmail.com>
2022-05-23 18:16:40 +0300

    task5

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,7 +40,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {
         client: `${name} ${phone}`, 
         order: {
-            address:`${street} ${house} ${entrance} ${floor} ${flat}`, 
+            address:`ул. ${street}, дом ${house}, ${entrance} подъезд, ${floor} этаж, кв ${flat}`, 
             sum
         }, 
         goods: [],
